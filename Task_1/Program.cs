778bf7d889cdc57ab811bc841667637202e70220
Nazar47Lysyk32 <137522819+Nazar47Lysyk32@users.noreply.github.com>
2023-12-11 19:34:19 +0200

    Add files via upload

---
+++ Task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+// Делегат для лямбда-функції фільтрації
+delegate bool FilterDelegate(int number);
+
+class Program
+{
+    static void Main()
+    {
+        // Створюємо масив цілих чисел
+        int[] numbers = { 2, 5, 12, 8, 3, 15, 7 };
+
+        // Викликаємо метод для фільтрації та виведення результату
+        FilterAndPrint(numbers, EvenFilter, "Even numbers:");
+        FilterAndPrint(numbers, OddFilter, "Odd numbers:");
+        FilterAndPrint(numbers, GreaterThanTenFilter, "Numbers greater than 10:");
+    }
+
+    // Лямбда-функції для фільтрації
+    static FilterDelegate EvenFilter = number => number % 2 == 0;
+    static FilterDelegate OddFilter = number => number % 2 != 0;
+    static FilterDelegate GreaterThanTenFilter = number => number > 10;
+
+    // Метод для фільтрації та виведення результату
+    static void FilterAndPrint(int[] numbers, FilterDelegate filter, string message)
+    {
+        Console.WriteLine(message);
+        foreach (var number in numbers)
+        {
+            if (filter(number))
+            {
+                Console.Write($"{number} ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
